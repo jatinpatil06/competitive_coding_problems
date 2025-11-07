774c91fdf958a58dde9164af058d9e51b0e66068
Jatin Patil <jatinpatil06@outlook.com>
2025-11-07 12:07:53 +0530

    dsa practise

--- Leetcode/5. Longest Palindromic Substring.cpp
+++ Leetcode/5. Longest Palindromic Substring.cpp
@@ -1,23 +1,58 @@
 class Solution {
 public:
-    string check(string s, int l, int r){
+    /*
+        Brute Force Approach with TC = O(N ^ 3)
+
+    bool isPalindrome(string &s, int l, int r){
+        while(l <= r){
+            if(s[l] != s[r])    return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+    string longestPalindrome(string s) {
         string longest = "";
-        while(l >= 0 && r < s.length() && s[l] == s[r]){
+        int max_len = 0, n = s.length();
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(isPalindrome(s, i, j)){
+                    if(j - i + 1 > max_len){
+                        max_len = j - i + 1;
+                        longest = s.substr(i, j - i + 1);
+                    }
+                }
+            }
+        }
+        return longest;
+    }
+        An Better approach would be to use two pointers with TC = O(N ^ 2)
+    */
+
+    string findLongest(string &s, int l, int r){
+        string longest = "";
+        while((l >= 0 && r < s.length()) && s[l] == s[r]){
             longest = s.substr(l, r - l + 1);
             l--;
             r++;
         }
         return longest;
     }
-    string longestPalindrome(string s) {
-        int n = s.size();
+    string longestPalindrome(string s){
+        int max_len = 0, n = s.length();
         string longest = "";
-        for(int i = 0; i < n; i++)
-        {
-            string temp = check(s, i, i);
-            if(temp.length() > longest.length())   longest = temp;
-            temp = check(s, i , i + 1);
-            if(temp.length() > longest.length())   longest = temp;
+        for(int i = 0; i < n; i++){
+            string temp;
+            temp = findLongest(s, i, i);
+            if(temp.length() > max_len){
+                longest = temp;
+                max_len = max(max_len, (int)longest.length());
+            } 
+            temp = findLongest(s, i - 1, i);
+            if(temp.length() > max_len){
+                longest = temp;
+                max_len = max(max_len, (int)longest.length());
+            } 
         }
         return longest;
     }
--- Leetcode/78. Subsets.cpp
+++ Leetcode/78. Subsets.cpp
@@ -12,5 +12,20 @@ public:
         vector<vector<int>> answers;
         generate({}, 0, nums.size(), nums, answers);
         return answers;
+
+        /*Optimized Approach using Bit Masking
+        vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> subsets;
+        int n = nums.size();
+        for(int mask = 0; mask < (1 << n); mask++){
+            vector<int> subset;
+            for(int i = 0; i < n; i++){
+                if(mask & (1 << i))    subset.push_back(nums[i]);
+            }
+            subsets.push_back(subset);
+        }
+        return subsets;
+    }
+        */
     }
 };

1e07c47a90a6569ba372e4f450a1cec60e034328
Jatin Patil <jatinpatil06@outlook.com>
2025-12-18 18:26:55 +0530

    starting revising arrays

--- Leetcode/189. Rotate Array.cpp
+++ Leetcode/189. Rotate Array.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    void swap(int &a, int &b){
-        int temp = b;
-        b = a;
-        a = temp;
+    void reverse_arr(vector<int> &arr, int l, int r){
+        while(l <= r){
+            swap(arr[l], arr[r]);
+            l++;
+            r--;
+        }
     }
     void rotate(vector<int>& nums, int k) {
-        /*
-        Brute Force Method
         int n = nums.size();
-        k = k % n;
-        for(int j = 0; j < k; j++){
+        k = k % n;  //rotating multiple times doesnt make sense
+        /*  Brute Force Approach
+        for(int x = 0; x < k; x++){
             int last = nums[n - 1];
-            for(int i = n - 1; i >= 1; i--) nums[i] = nums[i - 1];
+            for(int i = n - 1; i > 0; i--)  nums[i] = nums[i - 1];
             nums[0] = last;
         }
+            A better approach would be to store the right halves elements in a temporary vector TC = O(N) & SC = O(N)
+        vector<int> temp;
+        for(int i = n - k; i < n; i++)  temp.push_back(nums[i]);
+        for(int i = n - 1; i - k >= 0; i--) nums[i] = nums[i - k];
+        for(int i = 0; i < k; i++)  nums[i] = temp[i];
+
+            An optimal approach would be to reverse the arrays and rotate in place
         */
-        int n = nums.size();
-        k = k % n;
-        int i, j;
-        i = 0;
-        j = n - 1;
-        while(i <= j)   swap(nums[i++], nums[j--]);
-        i = 0;
-        j = k - 1;
-        while(i <= j)   swap(nums[i++], nums[j--]);
-        i = k;
-        j = n - 1;
-        while(i <= j)   swap(nums[i++], nums[j--]);
+        reverse_arr(nums, 0, n - 1);
+        reverse_arr(nums, 0, k - 1);
+        reverse_arr(nums, k, n - 1);
     }
 };
--- Leetcode/485. Max Consecutive Ones.cpp
+++ Leetcode/485. Max Consecutive Ones.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int>& nums) {
-        int l = -1, r = 0, max_len = 0;
-        for(; r < nums.size(); r++){
-            if(nums[r] == 0)    l = r;
-            max_len = max(max_len, r - l);
+        int max_len = 0, n = nums.size();
+        /*  Brute force approach with TC = O(N ^ 2)
+        for(int i = 0; i < n; i++){
+            int j = i;
+            while(j < n && nums[j] == 1){
+                max_len = max(j - i + 1, max_len);
+                j++;
+            }    
+        }
+            An efficient solution would be to use two pointers
+
+        */
+        int c = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == 0)    c = 0;
+            else{
+                c++;
+                max_len = max(max_len, c);
+            }    
         }
         return max_len;
     }
